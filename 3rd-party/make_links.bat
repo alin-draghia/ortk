51d59326c7752c127a7e156c4a89546d4d571104
alin-draghia <alin.draghia@gmail.com>
2014-04-10 15:09:08 +0300

    add code to remove the previous links before create

--- 3rd-party/make_links.bat
+++ 3rd-party/make_links.bat
@@ -1,3 +1,9 @@
+rmdir opencv
+rmdir eigen
+rmdir dlib
+rmdir poco
+rmdir vlfeat
+
 mklink /D opencv D:\SDK\OpenCV-2.4.8\opencv\my-build-x64\install
 mklink /D eigen D:\SDK\eigen-eigen-6b38706d90a9
 mklink /D dlib D:\SDK\dlib-18.4
