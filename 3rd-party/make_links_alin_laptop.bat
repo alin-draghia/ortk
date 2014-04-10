ae372b61891dc100595f399b35a896148aeec5a7
Alin Draghia <alin.draghia@gmail.com>
2014-04-10 21:23:00 +0300

    write script to configure 3rd Party links on my laptop

---
+++ 3rd-party/make_links_alin_laptop.bat
@@ -0,0 +1,11 @@
+rmdir opencv
+rmdir eigen
+rmdir dlib
+rmdir poco
+rmdir vlfeat
+
+mklink /D opencv d:\dev\opencv-2.4.8\opencv\my-build-x64\install
+mklink /D eigen d:\dev\eigen\eigen-eigen-6b38706d90a9
+mklink /D dlib d:\dev\dlib-18.6
+mklink /D poco d:\dev\poco-develop\my_build\install
+mklink /D vlfeat d:\dev\vlfeat-master
