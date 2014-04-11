ed3063c777c429aba6f004ae64653033480890aa
alin-draghia <alin.draghia@gmail.com>
2014-04-11 15:22:53 +0300

    Fix Dallal Triggs Hog Feature Extractor

--- src/object-recognition-toolkit/feature-extraction/dallal-trigs-hog.cpp
+++ src/object-recognition-toolkit/feature-extraction/dallal-trigs-hog.cpp
@@ -1,6 +1,7 @@
 #include "object-recognition-toolkit\feature-extraction\dallal-trigs-hog.h"
 
 #include <Poco\ClassLoader.h>
+#include <Poco\Path.h>
 
 namespace object_recognition_toolkit
 {
@@ -31,12 +32,12 @@ namespace
 		DallalTriggsHogRegistrar()
 		{
 			using object_recognition_toolkit::feature_extraction::DallalTriggsHog;
-
+			auto parent_dir = Poco::Path::current();
 			Poco::ClassLoader<object_recognition_toolkit::core::AlgorithmFactory> loader;
-			loader.loadLibrary("object-recognition-toolkit" ".dll");
+			loader.loadLibrary("object-recognition-toolkit.dll","AlgorithmFactoryManifest");
 			auto& factory = loader.instance("object_recognition_toolkit::core::AlgorithmFactory");
-			auto class_name = typeid(DallalTriggsHog).name();
-			factory.registerClass<DallalTriggsHog>(class_name);
+			//auto class_name = typeid(DallalTriggsHog).name();
+			factory.registerClass<DallalTriggsHog>("DallalTriggsHog");
 		}
 
 		~DallalTriggsHogRegistrar()
