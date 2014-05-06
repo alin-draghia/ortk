99229cf5ed8a4a98535faa6a3b8d381e9c19af6a
alin-draghia <alin.draghia@gmail.com>
2014-05-06 11:33:11 +0300

    created config.h file to enable some macro style meta programming

---
+++ include/object-recognition-toolkit/core/config.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef CONFIG_H_INCLUDED_
+#define CONFIG_H_INCLUDED_
+
+#define HAVE_OPENCV 1
+#define HAVE_VLFEAT 0
+#define HAVE_DLIB 0
+#define HAVE_EIGEN 0
+#define HAVE_MATIO 0
+#define HAVE_BOOST_SERIALIZE 1
+#define HAVE_BOOST_PYTHON 0
+
+#define HAVE_POCO 0
+
+
+
+#endif // CONFIG_H_INCLUDED_
