bcc90800d0bf67b4de81e363b34945bad472c10c
alin-draghia <alin.draghia@gmail.com>
2014-05-15 10:49:29 +0300

    write serialization code for some of the basic opencv types

---
+++ include/object-recognition-toolkit/core/serialization.h
@@ -0,0 +1,60 @@
+#pragma once
+#ifndef SERIALIZATION_H_INCLUDED_
+#define SERIALIZATION_H_INCLUDED_
+
+#include <boost/serialization/split_free.hpp>
+#include <opencv2/opencv.hpp>
+
+namespace boost
+{
+	namespace serialization
+	{
+		template<class Archive, class Tp>
+		void serialize(Archive& ar, cv::Size_<Tp>& size, const unsigned int version)
+		{
+			ar & size.width;
+			ar & size.height;
+		}
+
+		template<class Archive, class Tp>
+		void serialize(Archive& ar, cv::Point_<Tp>& point, const unsigned int version)
+		{
+			ar & point.x;
+			ar & point.y;
+		}
+
+		template<class Archive, class Tp>
+		void serialize(Archive& ar, cv::Rect_<Tp>& rect, const unsigned int version)
+		{
+			ar & rect.x;
+			ar & rect.y;
+			ar & rect.width;
+			ar & rect.height;
+		}
+
+
+		template<class Archive>
+		void save(Archive& ar, const cv::Mat& mat, const unsigned int version)
+		{
+			cv::FileStorage fs("", cv::FileStorage::WRITE + cv::FileStorage::FORMAT_YAML + cv::FileStorage::MEMORY);
+			fs << "mat_yaml" << mat;
+			std::string buffer = fs.releaseAndGetString();
+			ar << buffer;
+		}
+
+		template<class Archive>
+		void load(Archive& ar, cv::Mat& mat, const unsigned int version)
+		{
+			std::string buffer;
+			ar >> buffer;
+			cv::FileStorage fs(buffer, cv::FileStorage::READ + cv::FileStorage::FORMAT_YAML + cv::FileStorage::MEMORY);
+			ar["mat_yaml"] >> mat;
+		}
+
+	}
+}
+
+
+BOOST_SERIALIZATION_SPLIT_FREE(cv::Mat);
+
+#endif // SERIALIZATION_H_INCLUDED_
