32887ed98fcf5a97c394a70f87f7a0a2486a812a
alin-draghia <alin.draghia@gmail.com>
2014-06-18 14:23:20 +0300

    introspection trial

--- vs_proj/object-recognition-app/main.py
+++ vs_proj/object-recognition-app/main.py
@@ -2,12 +2,34 @@ from __future__ import division, print_function
 
 import sys
 import os
+import inspect
 
 from PySide import QtUiTools, QtGui
 
 from object_recognition_toolkit import *
+import my_detector
+import my_hog
+import my_svm
+
 
 def main():
+
+    
+    detector_subs = Detector.__subclasses__()
+
+    feature_extractor_subs = FeatureExtractor.__subclasses__()
+
+    nms_subs = NonMaximaSuppressor.__subclasses__()
+
+    pyramid_builder_subs = PyramidBuilder.__subclasses__()
+
+    image_scanner_subs = ImageScanner.__subclasses__()
+
+    classifier_subs = Classifier.__subclasses__()
+
+    classifier_trainer_subs = Trainer.__subclasses__()
+
+    detector_trainer_subs = DetectorTrainer.__subclasses__()
 
     #main application
     app = QtGui.QApplication(sys.argv)
