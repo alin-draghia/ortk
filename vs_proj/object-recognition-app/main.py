d954119a32ef5ecc45ec46f7042660e8f9a2ade3
alin-draghia <alin.draghia@gmail.com>
2014-06-18 17:57:34 +0300

    got the application somewhat working

--- vs_proj/object-recognition-app/main.py
+++ vs_proj/object-recognition-app/main.py
@@ -4,10 +4,21 @@ import sys
 import os
 import inspect
 import cv2
+import pickle
+import numpy as np
 
-from PySide import QtUiTools, QtGui
+import matplotlib
+matplotlib.use('Qt4Agg')
 
-import object_recognition_toolkit as my_toolkit
+import pylab
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg  as FigureCanvas
+from matplotlib.figure import Figure
+
+
+
+from PySide import QtUiTools, QtCore, QtGui
+
+import object_recognition_toolkit as objrec_tk
 
 
 def get_property_list(t):
@@ -19,9 +30,40 @@ class toolkit_object(object):
         self.name = name
 
 
+def make_detector():
+    pyramid_builder = objrec_tk.FloatPyramidBuilder(scale_factor=1.05, min_size=objrec_tk.Size(63,127), max_size=objrec_tk.Size())
+    image_scanner = objrec_tk.DenseImageScanner(win_size=objrec_tk.Size(64,128), win_step=objrec_tk.Size(8,8), padding=objrec_tk.Size())
+    nms = objrec_tk.PassThroughNms()
+    nms0 = objrec_tk.GroupRectanglesNms()
+    feature_extractor = None
+    classifier = None
+    with open(r'training_10\classifier_10.pkl', 'r') as f:
+        classifier = pickle.load(f)
+
+    with open(r'training_10\feature_extracotr_dbg.pkl', 'r') as f:
+        feature_extractor = pickle.load(f)
+
+    w = objrec_tk.VecF32()
+    w[:] = classifier.GetCoefs()[0, :]
+    b = np.asscalar(classifier.GetIntercept())
+
+    classifier_ = classifier
+    classifier = objrec_tk.LinearClassifier(b, w)
+
+    detector = objrec_tk.SlidingWindowDetector()
+    detector.pyramid_builder = pyramid_builder
+    detector.image_scanner = image_scanner
+    detector.feature_extractor = feature_extractor
+    detector.non_maxima_suppressor = nms
+    detector.classifier = classifier
+
+    return detector
+
+
 class TheApp(object):
 
     def __init__(self, main_window):
+        self.detector = make_detector()
         self.main_window = main_window
         self.main_window.actionImage.triggered.connect(self.open_image)
         self.main_window.actionOpen_Detector.triggered.connect(self.open_detector)
@@ -30,6 +72,7 @@ class TheApp(object):
         self.main_window.actionSave_Detector.triggered.connect(self.save_detector)
         self.configure_detector_dialog = None
         self.image = None
+
         return
 
     def open_image(self):
@@ -39,6 +82,26 @@ class TheApp(object):
             return
 
 
+        im = cv2.imread(filename, cv2.IMREAD_COLOR)
+        if im is None:
+            QtGui.QMessageBox.critical(self.main_window, "Open Image...", "Could not open the requested image!")
+            return
+
+        self.color_image = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+        self.gray_image = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        
+  
+        #self.qt_image = QtGui.QImage()
+        #self.qt_image.load(filename);
+        #self.main_window.image_label.setPixmap(QtGui.QPixmap.fromImage(self.qt_image))
+
+        self.fig = Figure()
+        ax = self.fig.add_subplot(111)
+        ax.imshow(self.color_image)
+        
+        canvas = FigureCanvas(self.fig)
+        self.main_window.setCentralWidget(canvas)
+
         return
 
     def open_detector(self):
@@ -46,6 +109,8 @@ class TheApp(object):
         filename, selected_filter = QtGui.QFileDialog.getOpenFileName(self.main_window, "Open Detector...")
         if not filename:
             return
+
+
 
         return
 
@@ -73,31 +138,54 @@ class TheApp(object):
 
     def run_detector(self):
         print('run detector...')
+
+        if(self.gray_image is None):
+            return
+
+        self.detection_boxex = objrec_tk.BoxVector()
+        self.detection_scores = objrec_tk.VecF64()
+        self.detector.Detect(self.gray_image, self.detection_boxex, self.detection_scores, 0.0)
+
+        diplay_image = np.copy(self.color_image)
+        for det in self.detection_boxex:
+            x = det.x
+            y = det.y
+            w = det.width
+            h = det.height
+            cv2.rectangle(diplay_image, (x,y), (x+w,y+h), (255,0,0))
+
+        self.fig = Figure()
+        ax = self.fig.add_subplot(111)
+        ax.imshow(diplay_image)
+        
+        canvas = FigureCanvas(self.fig)
+        self.main_window.setCentralWidget(canvas)
+
         return
 
 def main():
 
 
     pyramid_builders = []
-    pyramid_builders.append(toolkit_object(my_toolkit.FloatPyramidBuilder, "FloatPyramidBuilder"))
+    pyramid_builders.append(toolkit_object(objrec_tk.FloatPyramidBuilder, "FloatPyramidBuilder"))
 
     image_scanners = []
-    image_scanners.append(toolkit_object(my_toolkit.DenseImageScanner, "DenseImageScanner"))
+    image_scanners.append(toolkit_object(objrec_tk.DenseImageScanner, "DenseImageScanner"))
 
     feature_extractors = []
-    feature_extractors.append(toolkit_object(my_toolkit.HogExtractor, "Python HogExtractor"))
-    feature_extractors.append(toolkit_object(my_toolkit.HogFeatureExtractor, "C++ HogExtractor"))
+    feature_extractors.append(toolkit_object(objrec_tk.HogExtractor, "Python HogExtractor"))
+    feature_extractors.append(toolkit_object(objrec_tk.HogFeatureExtractor, "C++ HogExtractor"))
 
-    cls_subs = my_toolkit.Classifier.__subclasses__()
+    cls_subs = objrec_tk.Classifier.__subclasses__()
     classifiers = []
-    classifiers.append(toolkit_object(my_toolkit.MockPersonClassifier, "MockPersonClassifier"))
-    classifiers.append(toolkit_object(my_toolkit.LinearClassifier, "LinearClassifier"))
-    classifiers.append(toolkit_object(my_toolkit.LinearSVM_Classifier, "LinearSVM_Classifier"))
+    classifiers.append(toolkit_object(objrec_tk.MockPersonClassifier, "MockPersonClassifier"))
+    classifiers.append(toolkit_object(objrec_tk.LinearClassifier, "LinearClassifier"))
+    classifiers.append(toolkit_object(objrec_tk.LinearSVM_Classifier, "LinearSVM_Classifier"))
 
-    nms_subs = my_toolkit.NonMaximaSuppressor.__subclasses__()
+    nms_subs = objrec_tk.NonMaximaSuppressor.__subclasses__()
     nmss = []
-    nmss.append(toolkit_object(my_toolkit.PassThroughNms, "PassThroughNms"))
-    nmss.append(toolkit_object(my_toolkit.GroupRectanglesNms, "GroupRectanglesNms"))
+    nmss.append(toolkit_object(objrec_tk.PassThroughNms, "PassThroughNms"))
+    nmss.append(toolkit_object(objrec_tk.GroupRectanglesNms, "GroupRectanglesNms"))
     
 
     #main application
