ad199557cda8fc21ca75bece2dd436a8593ecfa4
alin-draghia <alin.draghia@gmail.com>
2014-06-18 10:48:54 +0300

    moved the feature extraction in such a way to reduce the memory allocation

--- src/object-recognition-toolkit/detection/detector_base.cpp
+++ src/object-recognition-toolkit/detection/detector_base.cpp
@@ -50,17 +50,20 @@ namespace object_recognition_toolkit
 
 			int pyramid_levels_count = pyramid.GetNumLevels();
 
-			std::vector<core::Matrix> window_images;
-			std::vector<core::Box> window_boxes;
-			core::Matrix features;
-			core::Matrix scores;
-
-
-			window_images.reserve(100000);
-			window_boxes.reserve(100000);
+			
 
 			for (int pyramid_level_index = 0; pyramid_level_index < pyramid_levels_count; pyramid_level_index++)
 			{
+
+				std::vector<core::Matrix> window_images;
+				std::vector<core::Box> window_boxes;
+				core::Matrix features;
+				core::Matrix scores;
+
+
+				window_images.reserve(100000);
+				window_boxes.reserve(100000);
+
 				const pyramid::PyramidLevel& pyramid_level = pyramid.GetLevel(pyramid_level_index);
 
 				std::vector<image_scanning::Window> windows;
@@ -73,22 +76,24 @@ namespace object_recognition_toolkit
 					window_images.push_back(windows[i].image);
 					window_boxes.push_back(pyramid_level.Invert(windows[i].box));
 				}
-			}
 
-			features.create(window_images.size(), featureExtractor_->Lenght(), CV_32F);
-			scores.create(window_images.size(), 1, CV_64F);
+				features.create(window_images.size(), featureExtractor_->Lenght(), CV_32F);
+				//scores.create(window_images.size(), 1, CV_64F);
 
-			featureExtractor_->ComputeMulti(window_images, features);
-			classifier_->PredictMulti(features, scores);
+				featureExtractor_->ComputeMulti(window_images, features);
+				scores = classifier_->PredictMulti(features);
 
-			for (int i = 0; i < scores.rows; i++)
-			{
-				if (scores.at<double>(i) > treshold) 
+				for (int i = 0; i < scores.rows; i++)
 				{
-					detections.push_back(window_boxes[i]);
-					confidences.push_back(scores.at<double>(i));
+					if (scores.at<double>(i) > treshold)
+					{
+						detections.push_back(window_boxes[i]);
+						confidences.push_back(scores.at<double>(i));
+					}
 				}
 			}
+
+
 			
 
 #if(0)
